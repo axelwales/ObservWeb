180a2344d094005d8eabd88e214207da574da810
Axel Wales <axelwales@gmail.com>
2017-04-07 15:55:41 -0500

    updated serializer

--- map/serializers.py
+++ map/serializers.py
@@ -1,13 +1,15 @@
 from rest_framework import serializers
 from map.models import Location, Fingerprint, AccessPoint
 
-class FingerprintSerializer(serializers.ModelSerializer):
-    access_point = NestedAccessPointSerializer(read_only=True)
-    location = NestedLocationSerializer(read_only=True)
-
+class NestedLocationSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Fingerprint
-        fields = ('id', 'location', 'access_point','rssi')
+        model = Location
+        fields = ('id', 'lat', 'lng')
+
+class NestedAccessPointSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = AccessPoint
+        fields = ('bssid')
 
 class NestedFingerprintSerializer(serializers.ModelSerializer):
     access_point = NestedAccessPointSerializer(read_only=True)
@@ -16,27 +18,6 @@ class NestedFingerprintSerializer(serializers.ModelSerializer):
         model = Fingerprint
         fields = ('id', 'access_point','rssi')
 
-class LocationFingerprintSerializer(serializers.ModelSerializer):
-	fingerprints = NestedFingerprintSerializer(many=True, read_only=True)
-
-    def create(self, validated_data):
-        fingerprints_data = validated_data.pop('fingerprints')
-        location = Location.objects.create(**validated_data)
-        for fingerprint_data in fingerprints_data:
-            access_point_data = fingerprint_data.pop('access_point')
-            AccessPoint.objects.create(**access_point_data)
-            Fingerprint.objects.create(location=location, **fingerprint_data)
-        return location
-
-    class Meta:
-        model = Location
-        fields = ('id', 'lat', 'lng', 'fingerprints')
-
-class NestedLocationSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Location
-        fields = ('id', 'lat', 'lng')
-
 class AccessPointSerializer(serializers.ModelSerializer):
     location = NestedLocationSerializer(read_only=True)
 
@@ -44,7 +25,50 @@ class AccessPointSerializer(serializers.ModelSerializer):
         model = AccessPoint
         fields = ('bssid', 'location')
 
-class NestedAccessPointSerializer(serializers.ModelSerializer):
+class FingerprintSerializer(serializers.ModelSerializer):
+    access_point = NestedAccessPointSerializer(read_only=True)
+    location = NestedLocationSerializer(read_only=True)
+
     class Meta:
-        model = AccessPoint
-        fields = ('bssid')
+        model = Fingerprint
+        fields = ('id', 'location', 'access_point','rssi')
+
+class LocationFingerprintSerializer(serializers.ModelSerializer):
+    fingerprint_set = NestedFingerprintSerializer(many=True)
+
+    def create(self, validated_data):
+        fingerprint_set_data = validated_data.pop('fingerprint_set')
+        location = Location.objects.create(lat=validated_data['lat'],lng=validated_data['lng'])
+        for fingerprint_data in fingerprint_set_data:
+            access_point_data = fingerprint_data.pop('access_point')
+            try:
+                access_point = AccessPoint.objects.get(pk=access_point_data['bssid'])
+            except AccessPoint.DoesNotExist:
+                access_point = AccessPoint.objects.create(**access_point_data)
+            Fingerprint.objects.create(location=location, access_point=access_point, **fingerprint_data)
+        return location
+
+    def update(self, instance, validated_data):
+
+        instance.lat = validated_data['lat']
+        instance.lng = validated_data['lng']
+        instance.save()
+
+        fingerprint_ids = [fingerprint['id'] for fingerprint in validated_data['fingerprint_set_data']]
+        for fingerprint in instance.fingerprint_set:
+            if fingerprint.id not in fingerprint_ids:
+                fingerprint.delete()
+
+        for fingerprint_data in validated_data['fingerprint_set_data']:
+            try:
+                access_point = AccessPoint.objects.get(pk=access_point_data['bssid'])
+            except AccessPoint.DoesNotExist:
+                access_point = AccessPoint.objects.create(bssid=validated_data['bssid'])
+            fingerprint = Fingerprint(id=fingerprint_data['id'], access_point=access_point, location=instance, rssi=fingerprint_data['rssi'])
+            fingerprint.save()
+
+        return instance
+
+    class Meta:
+        model = Location
+        fields = ('id', 'lat', 'lng', 'fingerprint_set')
