5a212c2b35a670575cca24060193b905b3906280
Axel Wales <axelwales@gmail.com>
2017-04-20 22:02:22 -0500

    added location estimation view and locator wrapper object

--- map/urls.py
+++ map/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^fingerprints/$', views.FingerprintList.as_view()),
     url(r'^fingerprints/(?P<pk>[0-9]+)/$', views.FingerprintDetail.as_view()),
+    url(r'^location/$', views.LocationEstimate.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- map/views.py
+++ map/views.py
@@ -1,16 +1,38 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from rest_framework import generics
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.parsers import JSONParser
 from map.models import Location
 from map.serializers import LocationFingerprintSerializer
+from map.locator_wrapper import LocatorWrapper
+
 
 def index(request):
     return render(request, 'index.html')
+
 
 class FingerprintList(generics.ListCreateAPIView):
     queryset = Location.objects.all()
     serializer_class = LocationFingerprintSerializer
 
+
 class FingerprintDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Location.objects.all()
     serializer_class = LocationFingerprintSerializer
+
+
+class LocationEstimate(APIView):
+    """
+    View to estimate location.
+    """
+
+    parser_classes = (JSONParser,)
+
+    def post(self, request):
+        """
+        Return a location estimate.
+        """
+        estimate = LocatorWrapper.get_estimate(request.data)
+        return Response(estimate)
