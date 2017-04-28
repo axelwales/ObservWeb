4786576a42d24bcf11e0d8dc54472832580c76b2
Axel Wales <axelwales@gmail.com>
2017-04-28 10:32:08 -0500

    added data massager

---
+++ map/accesspoint_massage.py
@@ -0,0 +1,37 @@
+from map.models import AccessPoint, Fingerprint, TempAccessPoint, TempFingerprint
+from django.db.models import Avg
+
+
+class Massager(object):
+
+    def __init__(self):
+        pass
+
+    def create_truncated_aps(self):
+        aps = AccessPoint.objects.all()
+        for ap in aps:
+            bssid = ap.bssid[:len(ap.bssid) - 1]
+            TempAccessPoint.objects.create(bssid=bssid)
+
+    def average_existing_fingerprints(self):
+        tempAPs = TempAccessPoint.objects.all()
+        for tap in tempAPs:
+            fingerprints = Fingerprint.objects.filter(access_point__contains=tap.bssid) \
+                .values('location') \
+                .annotate(rssi=Avg('rssi'))
+            for f in fingerprints:
+                TempFingerprint.objects.create(bssid=bssid, location=f['location'], rssi=f['rssi'])
+
+    def replace_aps(self):
+        tempAPs = TempAccessPoint.objects.all()
+        for tap in tempAPs:
+            AccessPoint.objects.create(bssid=tap.bssid)
+
+    def replace_fingerprint_aps(self):
+        tempAPs = TempAccessPoint.objects.all()
+        for tap in tempAPs:
+            access_point, created = AccessPoint.objects.get_or_create(bssid=tap.bssid)
+            fingerprints = Fingerprint.objects.filter(access_point__contains=tap.bssid)
+            for f in fingerprints:
+                f.access_point = access_point
+                f.save()
--- map/models.py
+++ map/models.py
@@ -14,10 +14,20 @@ class Location(models.Model):
         unique_together = ("lat", "lng")
 
 
+class TempAccessPoint(models.Model):
+    bssid = models.CharField(unique=True, max_length=17)
+
+
 class AccessPoint(models.Model):
-    bssid = models.CharField("MAC Address", unique=True, max_length=18)
+    bssid = models.CharField(unique=True, max_length=18)
     location = models.ForeignKey(Location, null=True, on_delete=models.SET_NULL)
     group = models.ForeignKey(DedicatedGroup, null=True, on_delete=models.SET_NULL)
+
+
+class TempFingerprint(models.Model):
+    access_point = models.ForeignKey(AccessPoint, db_index=True, null=True, on_delete=models.CASCADE)
+    location = models.ForeignKey(Location, db_index=True, null=True, on_delete=models.CASCADE)
+    rssi = models.IntegerField(null=True)
 
 
 class Fingerprint(models.Model):
@@ -31,7 +41,7 @@ class Fingerprint(models.Model):
         (WEST, "West"),
         (SOUTH, "South")
     )
-    access_point = models.ForeignKey(AccessPoint, null=True, on_delete=models.CASCADE)
+    access_point = models.ForeignKey(AccessPoint, db_index=True, null=True, on_delete=models.CASCADE)
     location = models.ForeignKey(Location, db_index=True, null=True, on_delete=models.CASCADE)
     rssi = models.IntegerField(null=True)
     direction = models.CharField(choices=DIRECTION_CHOICES, null=True, max_length=5)
